c3382e18699fb7ec3b0f5b94ac6d5f54f464556c
BrB93 <bryan.brik@coda-student.school>
2025-03-03 19:59:50 +0100

    Add models: Crop, Animal, and Farm classes

--- src/farm/Crop.java
+++ src/farm/Crop.java
@@ -1,4 +1,43 @@
 package farm;
 
 public class Crop {
+    private String name;
+    private int growthTime;
+    private int currentAge;
+    private boolean planted;
+    private double buyPrice;
+    private double sellPrice;
+
+    public Crop(String name, int growthTime, double buyPrice, double sellPrice) {
+        this.name = name;
+        this.growthTime = growthTime;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+        this.currentAge = 0;
+        this.planted = false;
+    }
+
+    public void plant() {
+        this.planted = true;
+    }
+
+    public void updateGrowth() {
+        if (planted) {
+            currentAge++;
+        }
+    }
+
+    public boolean isMature() {
+        return planted && currentAge >= growthTime;
+    }
+
+    public double getBuyPrice() {
+        return buyPrice;
+    }
+
+    public double getSellPrice() {
+        return isMature() ? sellPrice : 0;
+    }
+
+    // Autres getters/setters selon besoin
 }
--- src/farm/Farm.java
+++ src/farm/Farm.java
@@ -1,4 +1,58 @@
 package farm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Farm {
+    private double money;
+    private List<Crop> crops;
+    private List<Animal> animals;
+    private int maxPlots; // Nombre de parcelles disponibles
+
+    public Farm(double initialMoney, int maxPlots) {
+        this.money = initialMoney;
+        this.maxPlots = maxPlots;
+        this.crops = new ArrayList<>();
+        this.animals = new ArrayList<>();
+    }
+
+    public void buyCrop(Crop crop) {
+        if (money >= crop.getBuyPrice() && crops.size() < maxPlots) {
+            money -= crop.getBuyPrice();
+            crop.plant();
+            crops.add(crop);
+        }
+    }
+
+    public void buyAnimal(Animal animal) {
+        if (money >= animal.getBuyPrice()) {
+            money -= animal.getBuyPrice();
+            animals.add(animal);
+        }
+    }
+
+    public void sellCrop(Crop crop) {
+        if (crop.isMature()) {
+            money += crop.getSellPrice();
+            crops.remove(crop);
+        }
+    }
+
+    // Ex: vendre les produits animaux (pas encore détaillé)
+    public void sellAnimalProduct(double amount) {
+        money += amount;
+    }
+
+    public void updateAll() {
+        // Mettre à jour toutes les cultures
+        for (Crop crop : crops) {
+            crop.updateGrowth();
+        }
+        // Mettre à jour tous les animaux
+        for (Animal animal : animals) {
+            animal.updateState();
+        }
+    }
+
+    // Getters / setters ...
 }
